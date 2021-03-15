76bef292c5bf4f58a0dab4ddaeb925858983b428
Edwin Geovanny Bucay <42628058+wingeovanny@users.noreply.github.com>
2021-03-14 19:49:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 
 function App() {
+   
+  const [task, setTask] =useState("")
+
+  const addTask =(){
+    
+  }
+
   return (
+   
     <div className="container">
-      Hello name
+      <h1>Tareas</h1>
+      <hr/>
+      
+      <div className="row">
+
+        <div className="col-8" >
+          <h4 className="text-center">Lista de tareas</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+               <span className="lead" >Nombre de la tarea</span>
+               <button  className="btn btn-danger btn-sm float-right mx-2 " >Eliminar</button>
+              <button  className="btn btn-warning btn-sm float-right " >Editar</button>
+            </li>           
+          </ul>          
+        </div>
+        <div className="col-4" >
+          <h4 className="text-center" >Formulario</h4>
+          <form onSubmit={addTask}>
+            <input 
+               type="text"
+               className="form-control mb-2"
+               placeholder="Ingrese una tarea"
+               onChange={(text)=>setTask(text.target.value)}
+               value={task}
+            />
+
+            <button className="btn btn-dark btn-block" type="submit" >Agregar</button>
+            
+          </form>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
